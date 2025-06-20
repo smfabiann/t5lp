d97f60a1e5b050a14580f20fc3a97a8d232e8b02
smfabiann <smfabiann@gmail.com>
2025-06-20 18:34:10 -0400

    avance en p2

--- p1.pl
+++ p1.pl
@@ -1,39 +1,39 @@
-balancear(Lista, Balanceada) :-
-    balancear_aux(Lista, 0, [], Balanceada).
+balancear(Entrada, S) :-
+    balancear_aux(Entrada, 0, [], S).
 
 % Caso base: lista vacía, agregamos los ')' que falten
 balancear_aux([], 0, Acc, Acc).
-balancear_aux([], N, Acc, Balanceada) :- N > 0,
-    agregar_cierres(N, Acc, Balanceada).
+balancear_aux([], N, Acc, S) :- N > 0,
+    agregar_cierres(N, Acc, S).
 
 % Si encontramos '(', sumamos 1 al contador
-balancear_aux(['('|Resto], N, Acc, Balanceada) :-
+balancear_aux(['('|Resto], N, Acc, S) :-
     N1 is N + 1,
     append(Acc, ['('], NuevoAcc),
-    balancear_aux(Resto, N1, NuevoAcc, Balanceada).
+    balancear_aux(Resto, N1, NuevoAcc, S).
 
 % Si encontramos ')', restamos 1 al contador o agregamos '(' si es necesario
-balancear_aux([')'|Resto], N, Acc, Balanceada) :-
+balancear_aux([')'|Resto], N, Acc, S) :-
     N > 0,
     N1 is N - 1,
     append(Acc, [')'], NuevoAcc),
-    balancear_aux(Resto, N1, NuevoAcc, Balanceada).
+    balancear_aux(Resto, N1, NuevoAcc, S).
 
-balancear_aux([')'|Resto], 0, Acc, Balanceada) :-
+balancear_aux([')'|Resto], 0, Acc, S) :-
     append(Acc, ['(', ')'], NuevoAcc),
-    balancear_aux(Resto, 0, NuevoAcc, Balanceada).
+    balancear_aux(Resto, 0, NuevoAcc, S).
 
 % Ignora otros caracteres
-balancear_aux([_|Resto], N, Acc, Balanceada) :-
-    balancear_aux(Resto, N, Acc, Balanceada).
+balancear_aux([_|Resto], N, Acc, S) :-
+    balancear_aux(Resto, N, Acc, S).
 
 % Agrega N paréntesis de cierre al final
 agregar_cierres(0, Acc, Acc).
-agregar_cierres(N, Acc, Balanceada) :-
+agregar_cierres(N, Acc, S) :-
     N > 0,
     N1 is N - 1,
     append(Acc, [')'], NuevoAcc),
-    agregar_cierres(N1, NuevoAcc, Balanceada).
+    agregar_cierres(N1, NuevoAcc, S).
 
 /*
     ['(', ]]
